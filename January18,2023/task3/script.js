97d24771b44181fb5da8ae9a1d7525e1bc62a9b3
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-01-18 18:20:54 +0530

    practice tasks for Jan18,2023

--- January18,2023/task3/script.js
+++ January18,2023/task3/script.js
@@ -3,13 +3,13 @@ const loadHome = () => {
     let aboutBtn = document.getElementById("about-btn");
     let contactBtn = document.getElementById("contact-btn");
 
-    homeBtn.style.fontSize = "300%";
+    homeBtn.style.fontSize = "5vh";
     homeBtn.style.fontWeight = "700";
 
-    aboutBtn.style.fontSize = "150%";
+    aboutBtn.style.fontSize = "3vh";
     aboutBtn.style.fontWeight = "400";
 
-    contactBtn.style.fontSize = "150%";
+    contactBtn.style.fontSize = "3vh";
     contactBtn.style.fontWeight = "400";
 
     document.getElementById("page-body").innerHTML = `<p>No products are available for online shipment yet.</p>`;
@@ -20,13 +20,13 @@ const loadAbout = () => {
     let aboutBtn = document.getElementById("about-btn");
     let contactBtn = document.getElementById("contact-btn");
 
-    aboutBtn.style.fontSize = "300%";
+    aboutBtn.style.fontSize = "5vh";
     aboutBtn.style.fontWeight = "700";
 
-    homeBtn.style.fontSize = "150%";
+    homeBtn.style.fontSize = "3vh";
     homeBtn.style.fontWeight = "400";
 
-    contactBtn.style.fontSize = "150%";
+    contactBtn.style.fontSize = "3vh";
     contactBtn.style.fontWeight = "400";
  
     document.getElementById("page-body").innerHTML = `<p>MuscleTank® was founded with the idea of providing India with sports nutrition supplements that are specifically focused on Indian customer needs and listens to their sentiments day in day out. MuscleTank® has quickly risen as consumer’s favorite brand with industry-first concept introductions like authenticity, protein test certificates, scoop on top, clinically researched products, etc.</p>`;
@@ -37,13 +37,13 @@ const loadContact = () => {
     let aboutBtn = document.getElementById("about-btn");
     let contactBtn = document.getElementById("contact-btn");
 
-    contactBtn.style.fontSize = "300%";
+    contactBtn.style.fontSize = "5vh";
     contactBtn.style.fontWeight = "700";
 
-    homeBtn.style.fontSize = "150%";
+    homeBtn.style.fontSize = "3vh";
     homeBtn.style.fontWeight = "400";
 
-    aboutBtn.style.fontSize = "150%";
+    aboutBtn.style.fontSize = "3vh";
     aboutBtn.style.fontWeight = "400";
 
     document.getElementById("page-body").innerHTML = `<p><u>Write us at</u>: contact@muscletank.com</p>`;
