88ec6f3caf5e5b4d2ba5e8e15f05dd9578332fb1
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-01-23 16:36:14 +0530

    tasks for january23,2023

--- January23,2023/task2/script.js
+++ January23,2023/task2/script.js
@@ -2,3 +2,7 @@ alert("Welcome to this demo page!"); //method 1
 
 document.write("<h2>This heading was added using the document.write() method.</h2>");
 
+console.log("This was printed using console.log() method.")
+
+document.getElementById("printing-text").innerHTML = `<h2>This heading was printed using innnerHTML() method.</h2>`
+
---
+++ January23,2023/task3/script.js
@@ -0,0 +1,35 @@
+var checkPrime = (num) => {
+    let flag = true;
+    if(num==2){
+        return flag;
+    }
+    for(let i=2; i<=Math.ceil(Math.sqrt(num)); i++){
+        if(num%i==0){
+            flag = false;
+            break;
+        }
+    }
+    return flag
+}
+
+var showPrimes = (input, res) => {
+    let i = 2;
+    while(i<=input){
+        if(checkPrime(i)){
+            res.push(i);
+        }
+        i++;
+    }
+    return res;
+}
+
+function getNumber(){
+    let userInput = document.getElementById("number-field").value;
+    let result = showPrimes(userInput, []);
+    
+    document.getElementById("result-div").innerHTML = `<h1>${result}</h1>`;
+}
+
+
+
+
