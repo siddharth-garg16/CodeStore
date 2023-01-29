31bdad884e9cea59941b18d9dae6cb3b8f599b18
Siddharth Garg <80592648+siddharth-garg16@users.noreply.github.com>
2023-01-30 00:22:12 +0530

    Update index.js

--- January25,2023/task4/index.js
+++ January25,2023/task4/index.js
@@ -1,1 +1,39 @@
 //Interest calculater - Task 4
+
+class InterestCalculator {
+    constructor(principal, rate, time) {
+        this.principal = principal;
+        this.rate = rate;
+        this.time = time;
+    }
+  
+    static validate(principal, rate, time) {
+        if (principal <= 0) {
+            throw new Error("Principal amount should be greater than 0.");
+        }
+        if (rate <= 0) {
+            throw new Error("Interest rate should be greater than 0.");
+        }
+        if (time <= 0) {
+            throw new Error("Time should be greater than 0.");
+        }
+    }
+  
+    calculateSI() {
+        InterestCalculator.validate(this.principal, this.rate, this.time);
+        return (this.principal * this.rate * this.time) / 100;
+    }
+  
+    calculateCI() {
+        InterestCalculator.validate(this.principal, this.rate, this.time);
+        return this.principal * (Math.pow(1 + this.rate / 100, this.time));
+    }
+}
+  
+try {
+    const calculator = new InterestCalculator(10000, 7, 3);
+    console.log(calculator.calculateSI())
+    console.log(calculator.calculateCI())
+} catch (e) {
+    console.error(e);
+}
