0cb62265153240602d748e7210ea70346d76c86f
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-02-22 18:19:59 +0530

    lazy loading - creating modules, loadChild in module related paths

---
+++ February21,22/lazy/src/app/admin/admin.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { LoginComponent } from './login/login.component';
+import { ListComponent } from './list/list.component';
+
+console.log("Testing for admin module")
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    ListComponent
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }
---
+++ February21,22/lazy/src/app/user/user.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+import { LoginComponent } from './login/login.component';
+import { ListComponent } from './list/list.component';
+
+console.log("Testing for user module");
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    ListComponent
+  ],
+  imports: [
+    CommonModule,
+    UserRoutingModule
+  ]
+})
+export class UserModule { }
