75a5a731bae6ae9c38e008dc753149aaf98d54d1
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-02-23 12:22:06 +0530

    services in angular - joke app

--- February23,2023/service/src/app/app.module.ts
+++ February23,2023/service/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JokeComponent } from './joke/joke.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokeComponent
   ],
   imports: [
     BrowserModule,
---
+++ February23,2023/service/src/app/joke/joke.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-joke',
+  templateUrl: './joke.component.html',
+  styleUrls: ['./joke.component.css']
+})
+export class JokeComponent {
+  @Input() setup:string;
+  @Input() punchline:string;
+}
