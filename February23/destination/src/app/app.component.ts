cf7b7bbf1b5418d10d34836bdfef37d63a8f0579
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-02-23 17:07:24 +0530

    destination app

--- February23/destination/src/app/app.component.ts
+++ February23/destination/src/app/app.component.ts
@@ -7,10 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public cities:string[] = ["Delhi", "Mumbai", "Chandigarh", "Bangalore", "Hyderabad"]
+
   public routes = [
     {
       from:"",
       to:""
     }
   ]
+
+  currFrom:string = "";
+  currTo:string = "";
+
+  onChangeFrom(fromVal){
+    this.currFrom = fromVal.target.value
+  }
+  onChangeTo(toVal){
+    this.currTo = toVal.target.value
+  }
+
+  saveAndHandleRoutes(){
+    if(this.currFrom==="none" || this.currTo==="none" || this.currFrom === this.currTo){
+      return
+  }
+    this.routes.push({
+      from:this.currFrom,
+      to: this.currTo
+    })
+  }
+
+  clearSelections(){
+    this.routes.splice(1)
+  }
+
 }
