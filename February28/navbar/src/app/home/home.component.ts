5de43e5c9016bd72e40abcd350a1fc8118ffef28
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-03-09 18:23:04 +0530

    unit test and fix on feb28 task

--- February28/navbar/src/app/home/home.component.ts
+++ February28/navbar/src/app/home/home.component.ts
@@ -12,34 +12,19 @@ export class HomeComponent {
 
   arrivals:{imgPath:string, productName:string, productPrice:number}[]=[
     {
-      imgPath:"",
+      imgPath:"https://i0.wp.com/www.smartprix.com/bytes/wp-content/uploads/2022/09/geforce-rtx-4090-product-photo-002.webp?fit=1200%2C675&ssl=1",
       productName:"NVIDIA RTX 4090",
       productPrice: 249990
     },
     {
-      imgPath:"",
+      imgPath:"https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Untitled4475.png",
       productName:"SONY WH-1000XM5",
       productPrice: 34990
     },
     {
-      imgPath:"",
+      imgPath:"https://compumise.in/wp-content/uploads/2022/07/https___hybrismediaprod.blob_.core_.windows.net_sys-master-phoenix-images-container_h55_he9_9184349421598_iskur-x-gallery-1500x1000-06.jpg",
       productName:"RAZOR ISKUR",
       productPrice: 64990
-    },
-    {
-      imgPath:"",
-      productName:"NVIDIA RTX 4090",
-      productPrice: 250000
-    },
-    {
-      imgPath:"",
-      productName:"NVIDIA RTX 4090",
-      productPrice: 250000
-    },
-    {
-      imgPath:"",
-      productName:"NVIDIA RTX 4090",
-      productPrice: 250000
     }
   ]
 }
