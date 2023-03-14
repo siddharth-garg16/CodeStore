368dbd920c212110156083a021803f8204ba397e
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-03-14 12:34:19 +0530

    multi user app - jwt

---
+++ March13,14/multi-user-app/src/app/services/dashboard-guard.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardGuardService implements CanActivate{
+
+  constructor(private route: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean{
+    // if(localStorage.getItem('usertype')){
+    //   return true;
+    // }
+    if(localStorage.getItem('token')){
+      return true;
+    }
+    else{
+      // this.route.navigate(['/'])
+      return false;
+    }
+  }
+}
