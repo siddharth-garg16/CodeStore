435c37e2ede15afc9d0a506b272fc88049589e9d
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-03-17 16:57:50 +0530

    connectors - tenants app - fixes

--- March16/tenants/src/app/connector/connector.component.ts
+++ March16/tenants/src/app/connector/connector.component.ts
@@ -1,87 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-import {DataService} from '../Services/data.service';
-import {FormBuilder} from '@angular/forms';
-import {Tenant} from '../Models/tenant.model';
+import { DataService } from '../Services/data.service';
+import { FormBuilder } from '@angular/forms';
+import { Tenant } from '../Models/tenant.model';
 import { Observable } from 'rxjs';
+import {startWith, map} from 'rxjs/operators';
 
-export const _filter = (opt: string[], value: string): string[] => {
+export const _filter = (opt: {workflowName?:string, workflowID?:string}[], value: string): {workflowName?:string, workflowID?:string}[] => {
   const filterValue = value.toLowerCase();
-  return opt.filter(item => item.toLowerCase().includes(filterValue));
+  return opt.filter((item) => {
+    item.workflowName.toLowerCase()===filterValue;
+  });
 };
 
 @Component({
   selector: 'app-connector',
   templateUrl: './connector.component.html',
   styleUrls: ['./connector.component.scss'],
-  providers:[DataService]
+  providers: [DataService]
 })
-export class ConnectorComponent implements OnInit{
-  constructor(private dataService: DataService, private _formBuilder: FormBuilder){}
+export class ConnectorComponent implements OnInit {
+  constructor(private dataService: DataService, private _formBuilder: FormBuilder) { }
 
   importedData: any[];
   processedData: Tenant[] = [];
-  firstOptionVal:string;
-  secondOptionVal:string;
 
   tenantForm = this._formBuilder.group({
-    tenantGroup:''
+    tenantGroup: ''
   })
-
 
   tenantGroupOptions: Observable<Tenant[]>
 
-  ngOnInit(){
+  ngOnInit() {
     this.importedData = this.dataService.tenantData;
-
-    // for(let tenant of this.importedData){
-    //   if(tenant.crossTenantWorkflowSchemas.length>0){
-    //     for(let options of tenant.crossTenantWorkflowSchemas){
-    //       let currentTenant:Tenant = {
-    //         tenantName : tenant.tenantName,
-    //         workflowName: "",
-    //         workflowID: ""
-    //       };
-    //       // debugger
-    //       currentTenant.workflowName = options.name;
-    //       currentTenant.workflowID = options.workflowSchemaId;
-    //       this.processedData.push(currentTenant);
-    //     }
-    //   }
-    // }
-    for(let tenant of this.importedData){
-      if(tenant.crossTenantWorkflowSchemas.length>0){
-        let currentTenant:Tenant = {
+    for (let tenant of this.importedData) {
+      if (tenant.crossTenantWorkflowSchemas.length > 0) {
+        let currentTenant: Tenant = {
           tenantName: tenant.tenantName,
           workflow: []
         }
-        for(let options of tenant.crossTenantWorkflowSchemas){
-          currentTenant.workflow.push({workflowID:options.workflowSchemaId, workflowName: options.name});
+        for (let options of tenant.crossTenantWorkflowSchemas) {
+          currentTenant.workflow.push({ workflowID: options.workflowSchemaId, workflowName: options.name });
         }
         this.processedData.push(currentTenant);
       }
     }
 
+    this.tenantGroupOptions = this.tenantForm.get('tenantGroup')!.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filterGroup(value || ''))
+    )
   }
 
-  show(){
-    //testing
+  show() {
     console.log(this.importedData)
     console.log(this.processedData)
   }
 
-  showSelections(){
-    console.log(this.firstOptionVal, this.secondOptionVal)
+  showSelections() {
+    // console.log(this.firstOptionVal)
+    // console.log(this.firstOptionVal, this.secondOptionVal)
+    console.log(this.tenantForm.get('tenantGroup').value)
   }
 
 
   private _filterGroup(value: string): Tenant[] {
     if (value) {
-      return this.stateGroups
-        .map(group => ({letter: group.letter, names: _filter(group.names, value)}))
-        .filter(group => group.names.length > 0);
+      return this.processedData
+        .map(tenant => ({tenantName: tenant.tenantName, workflow: _filter(tenant.workflow, value)}))
+        .filter(tenant => tenant.workflow.length > 0);
     }
 
-    return this.stateGroups;
+    return this.processedData;
   }
-
 }
