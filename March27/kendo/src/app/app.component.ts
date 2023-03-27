0244b39f5668b5ef93013fc00d535230a53a7079
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-03-27 17:54:11 +0530

    kendoUI grid

---
+++ March27/kendo/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {customers} from './customers';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  public gridData: any[] = customers;
+}
